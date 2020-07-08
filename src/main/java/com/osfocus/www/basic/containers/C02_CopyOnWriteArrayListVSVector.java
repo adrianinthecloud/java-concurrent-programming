f9dc86f4c4210331dbb9ccebd25e633ee8df0ba5
Adrian LIU <adrian.liu.work@gmail.com>
2020-07-09 08:16:52 +1000

    Added: Insert performance comparison for CopyOnWriteList and Vector

---
+++ src/main/java/com/osfocus/www/basic/containers/C02_CopyOnWriteArrayListVSVector.java
@@ -0,0 +1,54 @@
+package com.osfocus.www.basic.containers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+// insert performance comparison
+public class C02_CopyOnWriteArrayListVSVector {
+    private static final int NUM_OF_THREADS = 100;
+    private static final int NUM_OF_ITEMS = 1000;
+
+    static void runAndComputeTime(Thread[] ths) {
+        long start = System.currentTimeMillis();
+        Arrays.asList(ths).forEach(t->t.start());
+        Arrays.asList(ths).forEach(t->{
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        long end = System.currentTimeMillis();
+        System.out.println("It took " + (end - start) + "ms");
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new CopyOnWriteArrayList<>();
+        Vector<Integer> vector = new Vector<>();
+
+        Thread[] threads = new Thread[NUM_OF_THREADS];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < NUM_OF_ITEMS; j++) {
+                    list.add((int) (1001 * Math.random()));
+                }
+            });
+        }
+
+        System.out.println("Performance of CopyOnWriteArrayList: ");
+        runAndComputeTime(threads);
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                vector.add((int) (1001 * Math.random()));
+            });
+        }
+
+        System.out.println("Performance of Vector: ");
+        runAndComputeTime(threads);
+    }
+}
